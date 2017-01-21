ea76ad36ec15befe57805998fa141562d26374db
Darul Ma'wa <mawadarul@gmail.com>
2017-01-21 22:03:36 +0700

    no message

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost22222',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'tn',
